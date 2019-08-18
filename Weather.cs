be228bdda8e8f86b0edba9cb68aefc2218d2ac2b
Maxim Babakov <mister.franz02@gmail.com>
2019-08-18 20:11:51 +0700

    Добавьте файлы проекта.

---
+++ Weather.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace OpenWeatherMapApi
+{
+    /// <summary>
+    /// Погода.
+    /// </summary>
+    public class Weather
+    {
+        /// <summary>
+        /// Город.
+        /// </summary>
+        public string City { get; }
+        /// <summary>
+        /// Температура.
+        /// </summary>
+        public int Temperature { get; }
+        /// <summary>
+        /// Давление.
+        /// </summary>
+        public int Pressure { get; }
+        /// <summary>
+        /// Влажность.
+        /// </summary>
+        public int Humidity { get; }
+        /// <summary>
+        /// Видимость.
+        /// </summary>
+        public int Visibility { get; }
+        /// <summary>
+        /// Скорость ветра.
+        /// </summary>
+        public double WindSpeed { get; }
+        /// <summary>
+        /// Облачность (в процентах).
+        /// </summary>
+        public int Clouds { get; }
+
+        /// <summary>
+        /// Создать новую погоду (полная версия).
+        /// </summary>
+        /// <param name="city">Город.</param>
+        /// <param name="temperature">Температура.</param>
+        /// <param name="pressure">Давление.</param>
+        /// <param name="humidity">Влажность.</param>
+        /// <param name="visibility">Видимость.</param>
+        /// <param name="windSpeed">Скорость ветра.</param>
+        /// <param name="clouds">Облачность (в процентах).</param>
+        public Weather(string city, int temperature, int pressure, int humidity, int visibility, double windSpeed, int clouds)
+        {
+            if (string.IsNullOrWhiteSpace(city))
+                throw new ArgumentNullException("Название города не может быть пустым.", nameof(city));
+            if (temperature > 100 || temperature < 100)
+                throw new ArgumentException("Некорректная температура.", nameof(temperature));
+            if (pressure < 0)
+                throw new ArgumentException("Некорректное давление.", nameof(pressure));
+            if (humidity < 0)
+                throw new ArgumentException("Некорректная влажность.", nameof(humidity));
+            if (visibility < 0)
+                throw new ArgumentException("Некорректная видимость.");
+            if (clouds < 0)
+                throw new ArgumentException("Некорректная облачность.");
+
+            City = city;
+            Temperature = temperature;
+            Pressure = pressure;
+            Humidity = humidity;
+            Visibility = visibility;
+            WindSpeed = windSpeed;
+            Clouds = clouds;
+        }
+
+        /// <summary>
+        /// Создать новую погоду (упрощённая версия).
+        /// </summary>
+        /// <param name="city">Город.</param>
+        /// <param name="temperature">Температура.</param>
+        /// <param name="clouds">Облачность.</param>
+        public Weather(string city, int temperature, int clouds)
+        {
+            if (string.IsNullOrWhiteSpace(city))
+                throw new ArgumentNullException("Название города не может быть пустым.", nameof(city));
+            if (temperature > 100 || temperature < 100)
+                throw new ArgumentException("Некорректная температура.", nameof(temperature));
+            if (clouds < 0)
+                throw new ArgumentException("Некорректная облачность.", nameof(clouds));
+
+            City = city;
+            Temperature = temperature;
+            Clouds = clouds;
+        }
+    }
+}
---
+++ WeatherController.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace OpenWeatherMapApi
+{
+    /// <summary>
+    /// Контроллер погоды.
+    /// </summary>
+    public class WeatherController
+    {
+        /// <summary>
+        /// Api ключ сервиса openweathermap
+        /// </summary>
+        public string ApiKey { get; }
+
+        /// <summary>
+        /// Создать новый контроллер погоды.
+        /// </summary>
+        /// <param name="apiKey">Ваш api ключ сервиса openweathermap.</param>
+        public WeatherController(string apiKey)
+        {
+            if (string.IsNullOrWhiteSpace(apiKey))
+                throw new ArgumentNullException("Api ключ не может быть пустым.", nameof(apiKey));
+
+            ApiKey = apiKey;
+        }
+
+        /// <summary>
+        /// Получить погоду в городе (полная информация).
+        /// </summary>
+        /// <param name="city">Город.</param>
+        /// <returns>Погода в городе.</returns>
+        public Weather GetWeather(string city)
+        {
+            string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={ApiKey}";
+
+            var data = GetRequest(url);
+
+            int temperature = data.main.temp;
+            int pressure = data.main.pressure;
+            int humidity = data.main.humidity;
+            int visibility = data.visibility;
+            double windSpeed = data.wind.speed;
+            int clouds = data.clouds.all;
+
+            return new Weather(city, temperature, pressure, humidity, visibility, windSpeed, clouds);
+        }
+
+        /// <summary>
+        /// Получить погоду в городе (упрощённая версия).
+        /// </summary>
+        /// <param name="city">Город.</param>
+        /// <returns>Погода в городе.</returns>
+        public Weather GetSimpleWeather(string city)
+        {
+            string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={ApiKey}";
+
+            var data = GetRequest(url);
+
+            int temperature = data.main.temp;
+            int clouds = data.clouds.all;
+
+            return new Weather(city, temperature, clouds);
+        }
+
+        /// <summary>
+        /// Отправить GET запрос к openweathermapapi.
+        /// </summary>
+        /// <param name="url">Адрес запроса.</param>
+        /// <returns>Данные в формате json.</returns>
+        private dynamic GetRequest(string url)
+        {
+            var httpClient = new HttpClient();
+
+            try
+            {
+                var json = httpClient.GetStringAsync(url).Result;
+                return JObject.Parse(json);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
